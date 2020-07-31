e4f6aed740ad4c018da05c53279295417fa1ff41
Michael Elgart <mje@michaelelgart.com>
2020-07-30 23:30:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -6,19 +6,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Michael Elgart</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm a software engineer, sci-fi fan, Duke CS/Econ grad, and IBMer. 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/michaelelgart/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
         </a>
+        <a
+          className="App-link"
+          href="https://twitter.com/mjelgart"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter
+        </a>
+
+        <a
+          className="App-link"
+          href="https://github.com/mjelgart"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+          className="App-link"
+          href="https://michaelelgart.com/blog"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Blog
+        </a>
+        <p>
+          I'm interested in, and occasionally blog about, effective altruism, sci-fi, and basketball.
+        </p>
       </header>
+      <body className="App-body">
+        <p>I'm currently redoing the site, so please pardon the dust. </p>
+      </body>
     </div>
   );
 }
